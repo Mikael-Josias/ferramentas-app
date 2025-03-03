0a47ca689224e4a19266be7b7ad2866df4b16e67
Mikael Josias <mikael.josias01@gmail.com>
2025-03-03 18:16:19 -0300

    feat(inverter-texto): add inversion methods

--- app/inverter-texto/page.tsx
+++ app/inverter-texto/page.tsx
@@ -1,18 +1,39 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
+
+const inversionMethods = ["Tudo", "Palavras"];
 
 export default function InvertText() {
   const [baseText, setBaseText] = useState("");
   const [resultText, setResultText] = useState("");
+  const [selectedMethod, setSelectedMethod] = useState<
+    (typeof inversionMethods)[number]
+  >(inversionMethods[0]);
 
   function invertText() {
-    setResultText([...baseText].reverse().join(""));
+    if (selectedMethod === "Tudo")
+      setResultText([...baseText].reverse().join(""));
+    if (selectedMethod === "Palavras")
+      setResultText(
+        baseText
+          .split(/(\s+)/)
+          .map((word) =>
+            /\S/.test(word) ? splitGraphemes(word).reverse().join("") : word,
+          )
+          .join(""),
+      );
+  }
+
+  function splitGraphemes(word: string): string[] {
+    return [...word];
   }
 
   return (
-    <section className="flex flex-col gap-6 px-5 py-10">
+    <section className="flex flex-col gap-6 md:px-10 px-5 py-10">
       <div>
         <h1 className="text-xl text-black font-bold">Inverter Texto</h1>
         <span className="text-sm text-gray-600">
@@ -23,7 +44,22 @@ export default function InvertText() {
           .
         </span>
       </div>
-      <div className="flex flex-col md:flex-row gap-3">
+      <div className="flex items-center gap-3">
+        <span>Método de Inversão:</span>
+        <RadioGroup defaultValue={inversionMethods[0]} className="flex">
+          {inversionMethods.map((m, i) => (
+            <Label key={i} htmlFor={m}>
+              <RadioGroupItem
+                value={m}
+                id={m}
+                onClick={() => setSelectedMethod(m)}
+              />
+              {m}
+            </Label>
+          ))}
+        </RadioGroup>
+      </div>
+      <div className="flex flex-col md:flex-row gap-3 md:gap-10">
         <div className="w-full">
           <span className="text-base text-gray-600">Texto base:</span>
           <Textarea
