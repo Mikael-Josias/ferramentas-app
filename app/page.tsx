eb364cb5660f974460aa5f6f5f1361297ef8d966
Mikael Josias <mikael.josias01@gmail.com>
2025-03-03 21:41:01 -0300

    feat(home): add text tools section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+import { ArrowDownZA } from "lucide-react";
+import Link from "next/link";
+
+const TOOLS_LINKS = [
+  {
+    type: "text",
+    name: "Inverter Texto",
+    link: "/inverter-texto",
+  },
+];
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <main className="px-5 md:px-10 py-20">
+      <section className="w-full flex flex-col gap-6">
+        <h4 className="text-xl text-black font-bold block">
+          Ferramentas de Texto
+        </h4>
+        <div className="flex gap-3 justify-between">
+          <Link
+            href="/inverter-texto"
+            className="flex flex-col items-center rounded-lg py-8 px-4 text-base text-gray-600 font-semibold w-full sm:w-[49%] md:w-[24%] bg-blue-50 hover:bg-blue-100 transition-colors text-center"
+          >
+            <ArrowDownZA size={64} className="mb-4" />
+            Inversor de Texto
+            <span className="text-sm font-normal">
+              inverte as letras de palavras e textos.
+            </span>
+          </Link>
+        </div>
+      </section>
+    </main>
   );
 }
