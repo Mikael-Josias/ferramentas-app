081fca2070c85b562077184a219b0b1aad46d6a1
Mikael Josias <mikael.josias01@gmail.com>
2025-03-10 20:43:50 -0300

    style(home): change style and icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,59 @@
-import { ALargeSmall, ArrowDownZA } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
+import TextToolsSVG from "./_assets/text-tools.svg";
+import InvertTextIcon from "./_assets/invert-text.svg";
+import UpperLower from "./_assets/upper-to-lower.svg";
 
 export default function Home() {
   return (
-    <main className="px-5 md:px-10 py-20 bg-gray-50">
-      <section className="w-full flex flex-col gap-6 p-12 bg-white rounded-4xl">
-        <h4 className="text-xl text-black font-bold block">
+    <main className="px-5 md:px-10 py-10 bg-gray-100">
+      <section className="w-full flex flex-col gap-3 p-5 bg-white">
+        <h4 className="flex items-center gap-2 text-2xl text-gray-700 font-bold pb-4 border-b-1 border-b-gray-100">
+          <Image
+            src={TextToolsSVG}
+            alt="white letters in a blue background"
+            className="h-9 w-auto pointer-events-none select-none"
+          />
           Ferramentas de Texto
         </h4>
-        <div className="flex flex-col md:flex-row  gap-3 justify-start">
+        <div className="flex flex-col flex-wrap px-4">
           <Link
             href="/inverter-texto"
-            className="flex flex-col items-center rounded-lg py-8 px-4 text-base text-gray-600 font-semibold w-full sm:w-[49%] md:w-[24%] bg-blue-50 hover:bg-blue-100 transition-colors text-center"
+            className="flex gap-3 p-1 items-start rounded-sm hover:bg-gray-50 transition-colors"
           >
-            <ArrowDownZA size={64} className="mb-4" />
-            Inversor de Texto
-            <span className="text-sm font-normal">
-              inverte as letras de palavras e textos.
-            </span>
+            <Image
+              src={InvertTextIcon}
+              width={40}
+              height={40}
+              alt="inverter texto icone"
+            />
+            <div className="flex flex-col gap-0">
+              <span className="text-base font-bold text-gray-800">
+                Inverter Texto
+              </span>
+              <span className="text-sm font-normal text-gray-600">
+                inverte as letras de palavras e textos.
+              </span>
+            </div>
           </Link>
           <Link
             href="/maiuscula-minuscula"
-            className="flex flex-col items-center rounded-lg py-8 px-4 text-base text-gray-600 font-semibold w-full sm:w-[49%] md:w-[24%] bg-blue-50 hover:bg-blue-100 transition-colors text-center"
+            className="flex gap-3 p-1 items-start rounded-sm hover:bg-gray-50 transition-colors"
           >
-            <ALargeSmall size={64} className="mb-4" />
-            Maiúsculas e Minúsculas
-            <span className="text-sm font-normal">
-              converta textos para maiúsculo, minúsculo ou somente a primeira
-              letra.
-            </span>
+            <Image
+              src={UpperLower}
+              width={40}
+              height={40}
+              alt="maiuscula e minuscula icone"
+            />
+            <div className="flex flex-col gap-0">
+              <span className="text-base font-bold text-gray-800">
+                Maiúsculas e Minúsculas
+              </span>
+              <span className="text-sm font-normal text-gray-600">
+                converta para maiúsculo, minúsculo ou a primeira letra
+              </span>
+            </div>
           </Link>
         </div>
       </section>
